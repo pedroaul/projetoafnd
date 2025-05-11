46b7eea54da6d4e9f30a0d524318bb49f1da9e55
pedroaul <red6190br@gmail.com>
2025-05-11 16:14:43 -0300

    addLine

--- src/components/create/index.js
+++ src/components/create/index.js
@@ -1,16 +1,18 @@
-import { ImNewTab } from 'react-icons/im';
 import './create.css';
 import { FaEraser, FaTrashAlt } from 'react-icons/fa';
-import { MdEditSquare } from 'react-icons/md';
+import { MdClear } from 'react-icons/md';
 import { IoIosSave } from 'react-icons/io';
 import { useEffect, useRef, useState } from 'react';
-import { Canvas, Circle } from 'fabric';
-
+import { Canvas, Circle, Line } from 'fabric';
+import { LiaPlusCircleSolid } from 'react-icons/lia';
 
 function Create() {
   
     const canvasRef = useRef(null);
     const [canvas, setCanvas] = useState(null);
+    const isDrawing = useRef(false);
+    const lineRef = useRef(null);
+    const startCoords = useRef({ x: 0, y: 0 });
 
     useEffect(() => {
         if(canvasRef.current) {
@@ -18,8 +20,37 @@ function Create() {
                 width: 1802,
                 height: 1021,
             });
-
+            initCanvas.backgroundColor = '#FFF';
             initCanvas.renderAll();
+
+        initCanvas.on('mouse:down', (opt) => {
+        isDrawing.current = true;
+        const pointer = initCanvas.getPointer(opt.e);
+        startCoords.current = { x: pointer.x, y: pointer.y };
+
+        const line = new Line([pointer.x, pointer.y, pointer.x, pointer.y], {
+          stroke: 'black',
+          strokeWidth: 2,
+          selectable: false,
+          evented: false,
+        });
+
+        lineRef.current = line;
+        initCanvas.add(line);
+      });
+
+      initCanvas.on('mouse:move', (opt) => {
+        if (!isDrawing.current || !lineRef.current) return;
+        const pointer = initCanvas.getPointer(opt.e);
+        lineRef.current.set({ x2: pointer.x, y2: pointer.y });
+        initCanvas.renderAll();
+      });
+
+      initCanvas.on('mouse:up', () => {
+        isDrawing.current = false;
+        lineRef.current.set({ selectable: true, evented: true }); // torna a linha selecionável
+        lineRef.current = null;
+      });
 
             setCanvas(initCanvas);
 
@@ -30,33 +61,37 @@ function Create() {
         }
     }, []);
 
-    const addCircle = ()=> {
+    const addCircle = () => {
         if(canvas) {
             const circle = new Circle({
                 top: 250,
                 left: 350,
                 radius: 50,
                 fill: "#F3CA40",
+                stroke: "#00000",
             });
             canvas.add(circle);
         }
     }
 
+    const remove = () => {
+        if(canvas) {
+            canvas.remove(canvas.getActiveObject());
+        }
+    }
+
     return(
         <div className='container'>
-            <div className="textSec">
+            <div className="workArea">
                 <h1 className='workTxt'>AGORA CRIE AQUI O SEU <span className='workSpan'>AUTÔMATO</span>!</h1>
                 <p className='workP'>Agora que entendemos um pouquinho sobre os autômatos chegou a hora de você fazer o seu. Bons estudos!</p>
-            </div>
-            <div className="workArea">
                 <div className='toolbar'>
-                    <div className='bt' id='circle' >{<ImNewTab onClick={addCircle} />}</div>
-                    <div className='bt' id='arrow'>{<FaEraser />}</div>
-                    <div className='bt' id='undo'>{<MdEditSquare />}</div>
+                    <div className='bt' id='circle' >{<LiaPlusCircleSolid onClick={addCircle} />}</div>
+                    <div className='bt' id='arrow'>{<MdClear onClick={remove}/>}</div>
                     <div className='bt'  id='bt_save'>{<IoIosSave />}</div>
                     <div className='bt' id='btTr'>{<FaTrashAlt />}</div>
                 </div>
-                <canvas id='canvas' ref={canvasRef} />
+                <canvas ref={canvasRef}  />
             </div>
         </div>
     );
