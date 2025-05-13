6325b4b3b04e12ea05e6c0775683d03aa5cc465b
pedroaul <red6190br@gmail.com>
2025-05-13 00:43:06 -0300

    new_layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,19 +3,15 @@ import './home.css';
 function Home() {
     return(
         <section className='homeSec'>
-                <h1 className='mainTitle'>CRIE SEU PRÓPRIO AUTÔMATO!</h1>
-                <div className='flexSection'>
-                <div className='txt'> 
-                        <h2 className='sub1'>Irá facilitar seus estudos, trabalho e entendimento de autômatos.</h2>
-                        <p className='homeP'>Aqui você poderá criar seus autômatos de maneira simples e prática que irão te salvar quando precisar entregar aquele trabalho caprichado e fácil de compreender. </p>
-                        <h2 className='sub2'>Não precisa mais fazer a moda antiga e desenhar tudo a mão! </h2>
-                        <p className='homeP'>Uma solução fácil e moderna de se usar para não perder mais tempo fazendo linhas e círculos com papel e caneta.</p>
-                    </div>
-                    <img src='../../images/automato.png' alt='automato' className='firstImage'></img>
+                <div className='txt'>
+                    <h1 className='mainTitle'>CRIE O SEU <br/> PRÓPRIO AUTÔMATO!</h1>
+                    <p className='homeP'>Aqui você poderá criar seus autômatos de maneira simples e prática que irão te <br/> salvar quando precisar entregar aquele trabalho caprichado e fácil de compreender. </p>
                 </div>
-                <div className='colorSec'>
-                    <h3 className='thirdSub'>Crie inúmeras possibilidades e salve onde quiser!</h3>
-                    <img src='../../images/criarautomato.jpg' className='secondImage'/>
+                <div className='prevSize'>
+                    <h2 className='prevSub'>Preview</h2>
+                </div>
+                <div className='preview'>
+                    <div className='prevEx'/>
                 </div>
         </section>
     );
